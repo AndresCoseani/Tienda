97442770489ed622d1128cf9f90e9e75dc6d44ce
root <mgiraudo@ies21.edu.ar>
2023-09-15 12:32:13 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tienda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tienda objProductos = new Tienda();
+            objProductos.grabar(Convert.ToInt32 (textBox1.Text), textBox2.Text,Convert.ToInt32 (textBox3.Text));//pasamos a grabar lo que hay en las txt
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int producto = 101;
+            string nombre = "TECLADO";
+            int stock = 11;
+
+            string sql = $"INSTERT INTO Productos VALUE ({producto}, '{nombre}', {stock})";//concatena todo con +",'"+
+            //interpolar: poner el $ antes de las comillas dobles para que el lenguaje tome el valor de las variables
+            //las variables van entre {} y las comillas simples van afuera de las llaves
+            MessageBox.Show(sql);
+        }
+    }
+}
---
+++ Tienda.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Tienda
+{
+    class Tienda
+    {
+         OleDbConnection conector;
+         OleDbCommand comando;
+         string sql;
+         public Tienda ()
+         {
+            conector = new OleDbConnection("provider=microsoft.jet.oledb.4.0;data source=TIENDA.mdb");
+            comando = new OleDbCommand();
+            sql = "";
+         }
+        public void grabar(int producto, string nombre, int stock)//pasamos esas cosas a grabar
+        {
+            sql = $"INSERT INTO Productos VALUES({producto}, '{nombre}', {stock})";//los strings siempre hay q ponerlos en comillas simples
+
+            conector.Open();//abre la base de datos
+            comando.Connection = conector; //indica a que base de datos le manda el comando
+            comando.CommandType = CommandType.Text;//indica el tipo de comando
+            comando.CommandText = sql; //indica cual es la tabla
+            comando.ExecuteNonQuery();//ejecuta el comando
+
+            conector.Close();//cierra la base de datos
+
+        }
+    }
+}
